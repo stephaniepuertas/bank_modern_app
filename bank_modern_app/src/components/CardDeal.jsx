0663268d0e9723dcd0de14a1b96e6160e3433ae1
stephaniepuertas <puertasstephanie@gmail.com>
2023-03-17 14:47:27 -0700

    editted button css and added card info

--- bank_modern_app/src/components/CardDeal.jsx
+++ bank_modern_app/src/components/CardDeal.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import {card} from '../assets';
+import styles, {layout} from '../style';
+import Button from './Button';
 
-const CardDeal = () => {
-  return (
-    <div>CardDeal</div>
-  )
-}
+const CardDeal = () => (
+    <section className={layout.section} >
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Fund a better card deal <br className='sm:block hidden'/> 
+        in few easy steps.</h2>
+        <p className={`${styles.paragraph} max-w-[460px] mt-5`}>Arcu torto, purus in mattis at sed integer facubus. Aliquet quis aliquet eget mauris toror. ç aliquet ultrices ac, ametau.</p>
+        <Button styles='mt-10'/>
+      </div>
+
+      <div className={layout.sectionImg}>
+        <img src={card} alt='card' className='w-[100%] h-[100%] ' />
+
+      </div>
+    </section>
+)
+
 
 export default CardDeal
