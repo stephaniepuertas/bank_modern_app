9cca8852ce6e3311dfa25acac2f368483f7e961a
stephaniepuertas <puertasstephanie@gmail.com>
2023-03-16 18:15:34 -0700

    finished hero section

--- bank_modern_app/src/components/Hero.jsx
+++ bank_modern_app/src/components/Hero.jsx
@@ -41,6 +41,11 @@ const Hero = () =>  (
         <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
         {/* gradient end */}
       </div>
+
+      <div className={`ss:hidden ${styles.flexCenter}`}>
+        <GetStarted/>
+
+      </div>
     </section >
   )
 
